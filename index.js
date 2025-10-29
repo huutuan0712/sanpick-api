3896a95fa2bb02acdea2956e67fe72705ca41bbc
TuanVo <tuan071220@gmail.com>
2025-10-29 17:35:57 +0700

    .

---
+++ index.js
@@ -0,0 +1,15 @@
+import { createApp } from "json-server/lib/app.js";
+import db from "./data.json" assert { type: "json" };
+
+const app = createApp(
+    {
+        data: db,
+    },
+    {
+        logger: true,
+    }
+);
+
+app.listen(process.env.PORT||3000, () => {
+    console.log("JSON Server is running");
+});
